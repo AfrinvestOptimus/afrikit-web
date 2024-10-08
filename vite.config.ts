b2cba6824897a0132535f79f404e715bb3b1e35b
infinitypaul <infinitypaul@live.com>
2024-10-08 22:23:36 +0100

    initial launch 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'path';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'AfrikitWeb',
+      fileName: (format) => `afrikit-web.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
