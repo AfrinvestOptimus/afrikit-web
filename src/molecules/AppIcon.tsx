e26150341aeeb7671e6ff87307f8ce4c87845a30
Amire Steve <zoe_steve@hotmail.com>
2024-11-10 19:40:44 +0100

    Refactor AppIcon component for improved flexibility and readability
    Simplify the AppIcon component by reordering and optimizing the classNames. This change enhances the flexibility and readability of the component's styling. Additionally, it introduces conditional classNames based on the size prop, resulting in more consistent and maintainable code.

--- src/molecules/AppIcon.tsx
+++ src/molecules/AppIcon.tsx
@@ -7,20 +7,21 @@ export type AppIconProps = {
   size: AppIconSize
 }
 
-const AppIcon: FC<AppIconProps> = ({
-  iconClassName,
-  size,
-}) => {
-
+const AppIcon: FC<AppIconProps> = ({ iconClassName, size }) => {
   return (
     <div
       className={clsx(
         Number(size) > 24
-          ? 'bg-light-surface-gray dark:bg-dark-surface-gray rounded-sm-max flex justify-center items-center'
+          ? 'flex items-center justify-center rounded-sm-max bg-light-surface-gray dark:bg-dark-surface-gray'
           : '',
-        size === '40' ? 'w-3xl p-sm' : size === '48' ? 'w-4xl p-md' : '',
+        size === '40' ? 'h-3xl w-3xl p-sm' : size === '48' ? 'h-4xl w-4xl p-md' : '',
       )}>
-      <i className={iconClassName}/>
+      <i
+        className={clsx(
+          iconClassName,
+          size === '16' ? 'text-base' : size === '20' ? 'text-xl' : 'text-2xl',
+        )}
+      />
     </div>
   )
 }
