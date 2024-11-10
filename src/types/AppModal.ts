7cb9b4f622b7a51017b70f0375f8fdac4970bfd8
Nkechi <kech3443@gmail.com>
2024-11-10 15:10:19 +0000

    Modal adjustment fix

--- src/types/AppModal.ts
+++ src/types/AppModal.ts
@@ -1,6 +1,6 @@
 export interface AppModalProps {
   isOpen: boolean // Boolean to control whether modal is open
-  onClose: () => void // Function to close the modal
+  onClose?: () => void // Function to close the modal
   onConfirm?: () => void // Optional function for confirm action
   title?: string // Title of the modal
   subtitle?: string // Optional subtitle
